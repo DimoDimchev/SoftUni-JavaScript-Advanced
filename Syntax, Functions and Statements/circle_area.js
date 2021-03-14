ffe350184ad4f6ed3d83f9e5362a5bc1e503d6ca
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-14 15:39:27 +0200

    Solved problem

--- Syntax, Functions and Statements/circle_area.js
+++ Syntax, Functions and Statements/circle_area.js
@@ -1,9 +1,9 @@
 function circleArea(radius) {
     if (typeof radius == 'number') {
-        let result = Math.PI * (radius ** 2)
-        console.log(result.toFixed(2))
+        let result = Math.PI * (radius ** 2);
+        console.log(result.toFixed(2));
     } else {
-        console.log(`We can not calculate the circle area, because we receive a ${typeof radius}.`)
+        console.log(`We can not calculate the circle area, because we receive a ${typeof radius}.`);
     }
 }
 
