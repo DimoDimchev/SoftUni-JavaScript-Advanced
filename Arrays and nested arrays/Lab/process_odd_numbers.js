76c43dbbe1f70e0d6f204e740c9eeaddcab326a1
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-15 19:07:17 +0200

    Solved problem

---
+++ Arrays and nested arrays/Lab/process_odd_numbers.js
@@ -0,0 +1,8 @@
+function findOddPositions(arr) {
+    return arr.filter((a, i) => i % 2 !== 0)
+        .map(x => x * 2)
+        .reverse()
+        .join(' ')
+}
+
+// console.log(findOddPositions([3, 0, 10, 4, 7, 3]))
