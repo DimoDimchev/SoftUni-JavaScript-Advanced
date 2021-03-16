5f187e82148b49285f1971bd20c11844c0f0f429
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-16 21:10:08 +0200

    Solved problem

---
+++ Arrays and nested arrays/Lab/biggest_element.js
@@ -0,0 +1,17 @@
+function findBiggestNumber(nestedArray) {
+    let biggestNumber = -(Number.MIN_VALUE);
+    let concatenatedArray = [];
+
+    nestedArray.forEach(function (element) {
+        concatenatedArray = concatenatedArray.concat(element);
+    })
+
+    concatenatedArray.sort(function(a, b){return a-b});
+    concatenatedArray.reverse();
+
+    return concatenatedArray[0];
+}
+
+// console.log(findBiggestNumber([[3, 5, 7, 12],
+//     [-1, 4, 33, 2],
+//     [8, 3, 0, 4]]))
