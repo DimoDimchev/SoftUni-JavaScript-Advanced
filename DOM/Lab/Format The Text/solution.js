4c1c272312b4329fbc0f7d3e01b39393dfd5e40c
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-20 13:29:04 +0200

    Solved problem

--- DOM/Lab/Format The Text/solution.js
+++ DOM/Lab/Format The Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
+    let text = document.getElementById('input').innerHTML;
+    let textList = text.split('.');
 
+    let output = document.getElementById('output');
+
+    for (let i = 0; i < textList.length; i += 3) {
+        let paragraph = document.createElement('p');
+        let str = '';
+
+        for (let j = 0; j < 3; j++) {
+            if (i + j < textList.length) {
+                str += textList[i + j] + '.';
+            }
+        }
+
+        paragraph.innerHTML = str;
+        output.appendChild(paragraph);
+    }
 }
