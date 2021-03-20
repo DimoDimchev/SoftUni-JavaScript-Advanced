543f3563024bdad6f9f9bd6defc757bb38f3e282
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-20 12:57:57 +0200

    Solved problem

--- DOM/Lab/Articles list/solution.js
+++ DOM/Lab/Articles list/solution.js
@@ -1,3 +1,23 @@
 function createArticle() {
+    let title = document.getElementById('createTitle');
+    let content = document.getElementById('createContent');
 
+    let articles = document.getElementById('articles');
+    let article = document.createElement('article');
+
+    let articleHeading = document.createElement('h3');
+    let articleContent = document.createElement('p');
+
+    if (title.value && content.value) {
+        articleHeading.innerHTML = title.value;
+        articleContent.innerHTML = content.value;
+
+        article.appendChild(articleHeading);
+        article.appendChild(articleContent);
+
+        articles.appendChild(article);
+
+        title.value = '';
+        content.value = '';
+    }
 }
