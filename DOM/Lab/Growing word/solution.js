e6d833de8091701600304a9946ceb746d4496d8d
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-20 14:21:47 +0200

    Created template

--- DOM/Lab/Growing word/solution.js
+++ DOM/Lab/Growing word/solution.js
@@ -1,3 +1,20 @@
 function growingWord() {
+    let paragraphToEdit = document.querySelector('#exercise > p');
+    let fontSize = 2;
 
+    let colorChanges = {
+        "blue": "green",
+        "green": "red",
+        "red": "blue"
+    };
+
+    if (!paragraphToEdit.hasAttribute('style')) {
+        paragraphToEdit.setAttribute("style", `color:blue; font-size: ${fontSize}px`);
+    } else {
+        let currentFontSize = paragraphToEdit.style['font-size'];
+        let currentColor = paragraphToEdit.style.color;
+        fontSize = currentFontSize.substr(0, currentFontSize.length - 2) * 2;
+
+        paragraphToEdit.setAttribute("style", `color:${colorChanges[currentColor]}; font-size: ${fontSize}px`);
+    }
 }
