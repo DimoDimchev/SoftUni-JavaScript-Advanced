56fe7bb647e05c4e07e42ecc2d0a9721c017b9df
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-20 19:41:39 +0200

    Solved problem

--- DOM/Exercise/Substraction/subtract.js
+++ DOM/Exercise/Substraction/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
+    let firstNumber = Number(document.getElementById('firstNumber').value);
+    let secondNumber = Number(document.getElementById('secondNumber').value);
+    let resultDiv = document.getElementById('result');
 
+    let result = firstNumber - secondNumber;
+
+    resultDiv.innerHTML = `${result}`;
 }
