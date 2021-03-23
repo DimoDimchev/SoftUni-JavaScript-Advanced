b3ff98a4c0f680af407112a8b10682b00f8007a1
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-23 23:53:43 +0200

    Created template and solved problem

---
+++ DOM Manipulations/Lab/Dynamic Validation/app.js
@@ -0,0 +1,13 @@
+function validate() {
+    let inputElement = document.getElementById('email');
+
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    inputElement.addEventListener('change', function(evt) {
+        if (regex.test(evt.target.value)) {
+            evt.target.removeAttribute('class');
+            return;
+        }
+        evt.target.className = 'error';
+    });
+}
