a7f04b22f4e32782d6af1de49a413fa416345aa3
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-31 23:08:51 +0300

    Solved problem

---
+++ JavaScript Advanced/Prototypes and inheritance/Lab/class_hierarchy.js
@@ -0,0 +1,87 @@
+function solve() {
+    class Figure {
+        constructor(units = 'cm') {
+            this.units = units;
+        }
+        changeUnits(newUnits) {
+            this.units = newUnits;
+        }
+        scaleParam(param) {
+            switch (this.units) {
+                case 'cm':
+                    break;
+                case 'mm':
+                    param = param * 10;
+                    break;
+                case "m":
+                    param = param / 10;
+                    break;
+                default:
+                    break;
+            }
+            return param;
+        }
+    }
+
+    class Circle extends Figure {
+        constructor(radius, units) {
+            super(units);
+            this._radius = radius;
+        }
+
+        get area() {
+            return Math.PI * this.radius * this.radius;
+        }
+
+        get radius() {
+            return this.scaleParam(this._radius);
+        }
+
+        toString() {
+            return `Figures units: ${this.units} Area: ${this.area} - radius: ${this.radius}`;
+        }
+    }
+
+    class Rectangle extends Figure {
+        constructor(width, height, units) {
+            super(units);
+            this._width = width;
+            this._height = height;
+        }
+
+        get area() {
+            return this.width * this.height;
+        }
+
+        get width() {
+            return this.scaleParam(this._width);
+        }
+
+        get height() {
+            return this.scaleParam(this._height);
+        }
+
+        toString() {
+            return `Figures units: ${this.units} Area: ${this.area} - width: ${this.width}, height: ${this.height}`;
+        }
+    }
+    return { Figure, Circle, Rectangle};
+}
+
+// let classes = solve();
+//
+// let c = new classes.Circle(5);
+// console.log(c.area); // 78.53981633974483
+// console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
+//
+// let r = new classes.Rectangle(3, 4, 'mm');
+// console.log(r.area); // 1200
+// console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
+//
+// r.changeUnits('cm');
+// console.log(r.area); // 12
+// console.log(r.toString()); // Figures units: cm Area: 12 - width: 3, height: 4
+//
+// c.changeUnits('mm');
+// console.log(c.area); // 7853.981633974483
+// console.log(c.toString()) // Figures units: mm Area: 7853.981633974483 - radius: 50
