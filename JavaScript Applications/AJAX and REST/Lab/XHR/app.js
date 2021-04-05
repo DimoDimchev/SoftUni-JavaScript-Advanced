36108658b747e103b309e74f1334f7fda2bb3057
Dimo Dimchev <ddimchev7@gmail.com>
2021-04-05 19:08:51 +0300

    Solved problem

--- JavaScript Applications/AJAX and REST/Lab/XHR/app.js
+++ JavaScript Applications/AJAX and REST/Lab/XHR/app.js
@@ -2,15 +2,16 @@ function loadRepos() {
     let resultDiv = document.getElementById('res');
     let loadButton = document.getElementsByTagName('button')[0];
 
-    let uri = "https://api.github.com/users/dimodimchev/repos"
-    let HttpRequest = new XMLHttpRequest();
-
-    HttpRequest.addEventListener('readystatechange', function () {
-        if (this.readyState === 4 && this.status === 200) {
-            resultDiv.textContent = this.responseText;
-        }
-    })
     loadButton.addEventListener('click', function () {
+        let uri = "https://api.github.com/users/dimodimchev/repos"
+        let HttpRequest = new XMLHttpRequest();
+
+        HttpRequest.addEventListener('readystatechange', function () {
+            if (this.readyState === 4 && this.status === 200) {
+                resultDiv.textContent = this.responseText;
+            }
+        })
+
         HttpRequest.open("GET", uri);
         HttpRequest.send();
     })
