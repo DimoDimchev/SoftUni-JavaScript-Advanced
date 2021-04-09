934a724be865fe393cd90c21268c81f3ca3ba887
Dimo Dimchev <ddimchev7@gmail.com>
2021-04-09 18:16:00 +0300

    Solved problem

---
+++ JavaScript Applications/Templating/Lab/Contacts/contacts.js
@@ -0,0 +1,20 @@
+const contacts = [
+    {
+        id: 1,
+        name: "John",
+        phoneNumber: 1234567,
+        email: "john@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Patricia",
+        phoneNumber: 7654321,
+        email: "patricia@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Bronhilda",
+        phoneNumber: 69666420,
+        email: "bronhilda@gmail.com"
+    }
+]
